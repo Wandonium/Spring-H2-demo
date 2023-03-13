1d368ee43c0be72f09ad8814eb1574f4668b0bb8
Wandonium <hillarywando@gmail.com>
2023-03-13 17:12:45 +0300

    Added my first test using Mockito and JUnit along with SpringBootTest. Added test for the departmentService.getByName method.

---
+++ src/test/java/com/wandonium/h2demo/service/DepartmentServiceTest.java
@@ -0,0 +1,41 @@
+package com.wandonium.h2demo.service;
+
+import com.wandonium.h2demo.entity.Department;
+import com.wandonium.h2demo.repository.DepartmentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DepartmentServiceTest {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @MockBean
+    private DepartmentRepository departmentRepository;
+
+    @BeforeEach
+    void setUp() {
+        Department department = Department.builder()
+                .departmentName("IT")
+                .departmentAddress("Lavington")
+                .departmentCode("IT-06")
+                .departmentId(1L)
+                .build();
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("IT"))
+                .thenReturn(department);
+    }
+
+    @Test
+    public void whenValidDepartmentName_ThenDepartmentShouldBeFound() {
+        String departmentName = "IT";
+        Department found = departmentService.getByName(departmentName);
+        assertEquals(departmentName, found.getDepartmentName());
+    }
+}
