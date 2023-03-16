6e45b571df10ffc1872c176747109bc552cd5799
Wandonium <hillarywando@gmail.com>
2023-03-16 15:58:29 +0300

    Added a dummy configs in the application.properties file and made use of them in the DummyController class.

--- src/main/java/com/wandonium/h2demo/controller/DummyController.java
+++ src/main/java/com/wandonium/h2demo/controller/DummyController.java
@@ -1,5 +1,6 @@
 package com.wandonium.h2demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DummyController {
 
+    @Value("${welcome.message}")
+    private String message;
+
     //@RequestMapping(value = "/", method = RequestMethod.GET)
     @GetMapping("/")
     public String helloWorld() {
-        return "Welcome to the Wandonium Cafe. Hello Universe!";
+        return message;
     }
 }
